77e8d26a8bdf32ce7615ed303340f411df96fb81
Sneha Farkya <63949465+snehafarkya@users.noreply.github.com>
2022-01-14 16:02:01 +0000

    created list sorting in python

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello, Welcome to SORT STATION! \nEnter integers to get sorted:")
+
+num = list(map(int,input().split()))
+print("After sorting in: ")
+num.sort()
+print("1. Ascending order: ",num)
+num.sort(reverse=True)
+print("2. Descending order: ",num) 
+
+print("\nEnter strings to get sorted: \n")
+num = list(map(str,input().split()))
+print("After sorting in: ")
+num.sort()
+print("1. Ascending order: ",num)
+num.sort(reverse=True)
+print("2. Descending order: ",num)
