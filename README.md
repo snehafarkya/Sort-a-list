112d153555b7cc569b718d81508dd939bc63f998
Sneha Farkya <63949465+snehafarkya@users.noreply.github.com>
2022-01-14 16:09:15 +0000

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Sort-a-list
+# Sort-a-list 
+<p align="center">
+  <img width="300" height="300" src="Sort.png">
+</p>
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# A python script to sort a list of integers as well as strings 
 print("Hello, Welcome to SORT STATION! \nEnter integers to get sorted:")
 
 num = list(map(int,input().split()))
